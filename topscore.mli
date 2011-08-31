3542d8c7b545751e1a241cc188fdfefc67cd5b1a
Martin Jambon <martin@mjambon.com>
2011-08-31 10:59:34 -0700

    Formatted comments for ocamldoc.

--- topscore.mli
+++ topscore.mli
@@ -1,27 +1,28 @@
-(*
+(**
   Accumulator of the top N values over a stream of values.
+
   Each value is associated with a score and each value is considered
   unique.
 *)
 
 type ('a, 'b) t
-  (* Accumulator of the top N values. *)
+  (** Accumulator of the top N values. *)
 
 val create : ('a -> 'a -> int) -> int -> 'a -> 'b -> ('a, 'b) t
-  (* 
+  (** 
      [create cmp n min_score dummy_data] returns an accumulator
      of the top [n] values of type ['b] based on their score of type ['a]
      and on the comparison function [cmp].
   *)
   
 val add : ('a, 'b) t -> 'a -> 'b -> unit
-  (* [add accu score data] *)
+  (** [add accu score data] *)
 
 val get : ('a, 'b) t -> int * ('a * 'b) array
-  (* Return the total count and the top N sorted (score, data) pairs. *)
+  (** Return the total count and the top N sorted (score, data) pairs. *)
 
 
-(* Specialized version for score of type int. *)
+(** Specialized version for score of type int. *)
 module Int :
 sig
   type 'a t
@@ -30,7 +31,7 @@ sig
   val get : 'a t -> int * (int * 'a) array
 end
 
-(* Specialized version for score of type float. *)
+(** Specialized version for score of type float. *)
 module Float :
 sig
   type 'a t
